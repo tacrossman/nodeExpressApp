a16893502a8c8510d65ecc33ce4ef9b4e40a0808
thomas <hello@thomascrossman.co.uk>
2013-01-27 16:31:40 +0000

    add in middleware (methodOverride + static + route)

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + "/public"));
 });
 
 app.get("/", function(req, res){

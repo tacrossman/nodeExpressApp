f9ab755f12d974bf411abccc2b05a7bf74fbd6bf
thomas <hello@thomascrossman.co.uk>
2013-01-27 17:09:53 +0000

    setting up and exploring responses

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + "/public"));
+	//app settings
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+
+	//use includes
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + "/public"));
 });
 
 app.get("/", function(req, res){
@@ -19,6 +22,25 @@ app.get("/", function(req, res){
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+app.get("/agent", function (req, res){
+	//get user agent string
+	//res.send(req.get('user-agent'));
+	// check content types
+	//res.send(req.accepts( [  'html', 'text', 'json' ] ));
+	// can i accept utf8?
+	//res.send(req.acceptsCharset('utf-8') ? 'yes' : 'no');
+	//can i accept french? (acceptd languages check)
+	res.send(req.acceptsLanguage('fr') ? 'yes' : 'no');
+});
+
+app.get("/name/:name?", function (req, res){
+	//print out name from URL
+	//res.send(req.params.name);
+	//setup default vaule for route
+	res.send(req.param('name', 'No name setup!'));
+	
 });
 
 

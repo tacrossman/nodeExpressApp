266f9136491b183f84f8007016a642580703975b
thomas <hello@thomascrossman.co.uk>
2013-01-27 17:47:07 +0000

    tracking downloads and different ways of writing get functions

--- app.js
+++ app.js
@@ -8,12 +8,63 @@ app.configure(function(){
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
-
-	//use includes
+	//use includes middleware
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
 	app.use(express.static(__dirname + "/public"));
+});
+
+// new users array
+var users = [
+	{ name: 'thomas'},
+	{ name: 'jana'}
+];
+
+function loadUser (req, res, next){
+	req.user = users[parseInt(req.params.userId, 10)];
+	next();
+}
+
+app.get("/users/:userId", loadUser, function(req, res){
+	//get user
+	res.json(req.user);
+});
+
+//start counter for tracking downloads
+var count = 0;
+
+app.get('/hello.txt', function(req, res, next){
+	//can override the text via custom route
+	//res.send('boom');
+	//increment the counter to track potential donwloads of hello.text
+	count++;
+	next();
+})
+
+app.get('/count', function(req, res){
+	//print the ammount of views/downloads of above hello.txt to /count route
+	res.send("" + count + " views");
+});
+
+//users variable
+var users2 = ['jane', 'tom', 'dave', 'john', 'jane'];
+
+app.param('from', function(req, res, next, from){
+	req.from = parseInt(from, 10);
+	//passing the function to the next middleware layer
+	next();
+});
+
+app.param('to', function(req, res, next, to){
+	req.to = parseInt(to, 10);
+	//passing the function to the next middleware layer
+	next();
+});
+
+app.get('/users/:from-:to', function(req, res){
+	//return the users in the json i.e /users/0-1
+	res.json(users2.slice(req.from, req.to + 1));
 });
 
 app.get("/", function(req, res){
@@ -59,8 +110,8 @@ app.get("/json", function(req, res){
 app.get('/home', function(req, res){
 	//redirect any /home url to root
 	//res.redirect(302, "/");
-	//response status
-	//res.status(302).redirect("/");
+	//response status redirect
+	res.status(302).redirect("/");
 });
 
 // app.get("/hi", function(req, res){
